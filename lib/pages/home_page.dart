13858aa1100528afb4f2d870581c867ea1a9b451
JT Rael <raeljohntimothy@gmail.com>
2024-04-17 17:43:47 +0800

    GNav setup

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_shop/components/bottom_nav_bar.dart';
+import 'package:shoe_shop/pages/cart_page.dart';
+import 'package:shoe_shop/pages/shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // Will update the selected index
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // List of pages
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CustomBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+    );
   }
 }
